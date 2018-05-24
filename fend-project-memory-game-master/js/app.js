7402e7cb8870b93c9f344712f749394cb1a2a813
James Townsend <jtownsend@pc.factset.com>
2018-05-24 08:23:59 +0100

    Added scoring system

--- fend-project-memory-game-master/js/app.js
+++ fend-project-memory-game-master/js/app.js
@@ -1,30 +1,33 @@
-/*
- * Create a list that holds all of your cards
- */
 
+// Create a list that holds all of your cards
 let cardsList = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt",
  "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb",
  "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"],
 
-    deck = $( ".deck" );
-    wait = 450
+    deck = $( ".deck" ),
+    delay = 500,
+    match = 0,
+    count = 0,
+    moves = 0;
+    cardsTotal = cardsList.length / 2;
 
-// *   - add each card's HTML to the page
+// add each card's HTML to the page
 function startGame() {
     deck.empty();
+    match = 0;
+    moves = 0;
+    $( ".moves" ).text(moves);
 
-// - shuffle the list of cards using the provided "shuffle" method below
+// shuffle the list of cards using the provided "shuffle" method below
     let shuffleCards = shuffle(cardsList);
 
-// - loop through each card and create its HTML
+// loop through each card and create its HTML
     for (var i =0; i < shuffleCards.length; i++) {
         deck.append($( "<li class=\"card\"><i class=\"" + shuffleCards[i] + "\"></i></li>" ));
-   }
+    }
 
- // set up the event listener for a card. If a card is clicked:
-
- addCardListener()
-
+// set up the event listener for a card. If a card is clicked:
+    addCardListener()
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -38,58 +41,77 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
-    return array;
+  return array;
 }
 
+// Determines score of player.
+function rating(moves) {
+    let rating = 3;
+    if (moves > 10 && moves < 15) {
+        $('.fa-star').eq(3).removeClass('fa-star').addClass('fa-star-o');
+    } else if (moves > 15 && moves < 20) {
+        $('.fa-star').eq(2).removeClass('fa-star').addClass('fa-star-o');
+    } else if (moves > 20) {
+        $('.fa-star').eq(1).removeClass('fa-star').addClass('fa-star-o');
+        rating = 1;
+    }
+    return { score: rating };
+}
 
- // set up the event listener for a card. If a card is clicked:
+// restarts the game on click.
+$( ".restart" ).on("click", function(){
+  startGame();
+});
+
+
+// set up the event listener for a card. If a card is clicked:
 let openCards = [];
 
 let addCardListener = function () {
-     // openCards = [];
      $( ".card" ).on("click", function(){
-
-     //if ($(this).hasClass('show') || $(this).hasClass('match')) { return true; }
-// *  - display the card's symbol (put this functionality in another function that you call from this one)
+// display the card's symbol (put this functionality in another function that you call from this one)
      let card = $(this).html();
      $(this).addClass("open show");
- // *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+// add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
      openCards.push(card);
 
+// if the list already has another card, check to see if the two cards match
+// if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
      if (openCards.length > 1) {
-         if (card === openCards[0]) {
+        if (card === openCards[0]) {
            $( ".open" ).addClass("match");
            setTimeout(function () {
               $( ".open" ).removeClass("open show");
-          }, wait);
-          openCards = [];
+          }, delay);
+          match ++;
 
+// if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
        } else {
          $( ".open" ).addClass("notmatch");
          setTimeout(function () {
              $( ".open" ).removeClass("open show");
-         }, wait);
-         openCards = [];
-
-
-
+         }, delay);
      }
 
-
+     openCards = [];
+     moves ++;
+     rating(moves);
+     $( ".moves" ).text(moves);
 
  }
- // *  - if the list already has another card, check to see if the two cards match
-});
+
+ // if cardsTotal === match) {
+ //   rating(moves)
+ });
+
 }
+
 
 startGame()
 
- // *  - display the card's symbol (put this functionality in another function that you call from this one)
- // *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- // *  - if the list already has another card, check to see if the two cards match
- // *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- // *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+
+
+
  // *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  // *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  // */
